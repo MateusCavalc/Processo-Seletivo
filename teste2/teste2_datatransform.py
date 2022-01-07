7e0325e63742abbf0ec8896f419a47807186c1ef
MateusCavalc <72281215+MateusCavalc@users.noreply.github.com>
2022-01-07 14:54:21 -0300

    Update teste2_datatransform.py

--- teste2/teste2_datatransform.py
+++ teste2/teste2_datatransform.py
@@ -5,15 +5,15 @@ from zipfile import ZipFile
 
 FILE = "padrao-tiss_componente-organizacional_202111.pdf"
 
-def get_tables(filename):
+def get_tables(filename): # Recebe o nome do arquivo .pdf e efetua a leitura das tabela, retornando a estrutura com as informações das páginas 114-120 (tabelas 30, 31 e 32)
     print('\nImporting tables from file \'' + filename + '\' ...')
     return camelot.read_pdf(filename, pages='114-120', flavor='lattice', line_scale=40, strip_text=' .\n')
 
-def merge_tables(tables, a, b):
+def merge_tables(tables, a, b): # Recebe uma lista de tabelas e efetua a concatenação das tabelas entre 'tabelas[a]' e 'tabelas[b]'
     print('Merging tables ...')
     return pd.concat([tables[i].df for i in range(a, b+1)], ignore_index=True)
 
-def save_csv(table, filename):
+def save_csv(table, filename): # Recebe a tabela e salva no arquivo 'filename'
     print('Saving table to file \'' + filename + '\' ...')
     table.to_csv(filename)
 
@@ -21,17 +21,18 @@ def main():
     tables = get_tables(FILE)
     print('Found ' + str(tables.n) + ' tables')
 
-    merged_table = merge_tables(tables, 1, 6)
+    merged_table = merge_tables(tables, 1, 6) # Tabela 31 do arquivo .pdf se encontra segmentada nas tabelas tables[1] - tables[6]
     
-    save_csv(tables[0], 'table_1.csv')  
+    save_csv(tables[0], 'table_1.csv') # Salva tabela 30 no arquivo 'table_1.csv'
     # camelot.plot(tables[0], kind='grid').show()
 
-    save_csv(merged_table, 'table_2.csv') 
+    save_csv(merged_table, 'table_2.csv') # Salva tabela 31 no arquivo 'table_2.csv'
     # camelot.plot(merged_table, kind='grid').show()
 
-    save_csv(tables[7], 'table_3.csv')  
+    save_csv(tables[7], 'table_3.csv') # Salva tabela 32 no arquivo 'table_3.csv'
     # camelot.plot(tables[7], kind='grid').show()
 
+    # Cria o .zip com os arquivos .csv gerados
     zipObj = ZipFile('Teste_Mateus.zip', 'w')
     zipObj.write('table_1.csv')
     zipObj.write('table_2.csv')
